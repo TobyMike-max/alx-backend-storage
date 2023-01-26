fd97a2006b393a08ecc412b4e39ebd4323d80cae
TobyMike-max <osinubi.michael@gmail.com>
2023-01-26 09:31:40 +0100

    [Modified]: Added quotes round set keyword

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,6 +6,6 @@ def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on name"""
     mongo_collection.update(
       {"name": name},
-      {$set: {"topics": topics}},
+      {"$set": {"topics": topics}},
       {multi: true}
     )
