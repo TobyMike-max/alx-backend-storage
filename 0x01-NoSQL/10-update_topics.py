22a0c79371130ab1b1ea933a5bf54a293f044dfe
TobyMike-max <osinubi.michael@gmail.com>
2023-01-26 09:21:58 +0100

    [Modified]: Changed collection methods name

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,4 +4,8 @@
 
 def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on name"""
-    mongo_collection.update_many({"name": name}, {$set: {"topics": topics}})
+    mongo_collection.update(
+      {"name": name},
+      {$set: {"topics": topics}},
+      {multi: true}
+    )
